13fcd80fe744e3930d62f7f151dec2fe749507a0
developer <developer@fatporkrinds.com>
2024-05-13 04:40:17 -0500

    Fixed problem with Debian and Docker

--- Makefile.Bookworm.mk
+++ Makefile.Bookworm.mk
@@ -4,7 +4,7 @@ DIDYOUKNOW = 'DevOps Tip: Infrastructure as Code tools like Terraform let you ma
     'Did you know? Git\'s distributed nature makes collaboration a breeze.' \
     'Htop: Because sometimes you need to see what\'s REALLY going on with your system.'
 
-PACKAGES = make wget curl jq qemu-system-x86 qemu-utils cloud-guest-utils cloud-init virtualbox cloud-utils build-essential virtualbox libvirt-daemon-system libvirt-clients libvirt-daemon bridge-utils virt-manager libguestfs-tools libosinfo-bin libguestfs-tools virt-top virtinst libvirt-doc libz-dev gnome-terminal python3.11-venv vim vim-gtk3 figlet unzip fail2ban clamav libpng-dev libjpeg-dev default-jdk libgl1-mesa-glx libegl1-mesa libxrandr2 libxrandr2 libxss1 libxcursor1 libxcomposite1 libasound2t64 libxi6 libxtst6
+PACKAGES = make wget curl cgroupfs-mount qemu-system-x86 qemu-utils cloud-guest-utils cloud-init virtualbox cloud-utils build-essential virtualbox libvirt-daemon-system libvirt-clients libvirt-daemon bridge-utils virt-manager libguestfs-tools libosinfo-bin libguestfs-tools virt-top virtinst libvirt-doc libz-dev gnome-terminal python3.11-venv vim vim-gtk3 figlet unzip fail2ban clamav libpng-dev libjpeg-dev default-jdk libgl1-mesa-glx libegl1-mesa libxrandr2 libxrandr2 libxss1 libxcursor1 libxcomposite1 libasound2t64 libxi6 libxtst6
 
 setup-bookworm:
 ifeq ($(shell uname),Darwin)
